154e1589d102e9a41009772f0c7156fe0c015d40
Sankhya Siddesh <siddeshsankhya@gmail.com>
2022-09-16 18:34:42 +0530

    first2@

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { WagmiConfig, createClient,useAccount  } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
+import { useEffect,useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const alchemyId = process.env.ALCHEMY_ID;
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Splitzy",
+    alchemyId,
+  }),
+);
+
+const Hero = ()=>{
+ 
+  const [transactionsData, setTransactionData] = useState([]);
+  const { address} = useAccount();
+  const url=`https://api.covalenthq.com/v1/1/address/${address}/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=false&key=ckey_54cbd696cf284627b6382f6832c`
+  useEffect(() => {
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {setTransactionData(json['data']['items']);console.log(json['data']['items']);})
+    }, []);
+  return(
+    <div>
+      {transactionsData.map((transaction)=>(
+        transaction.value !== "0" ? <h1>{transaction.to_address} - {transaction.value/(1000000000000000000)}</h1> : <h1></h1>
+      ))}
     </div>
-  );
+  )
 }
 
+
+const App = () => {
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Hero />
+        <ConnectKitButton />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
+};
 export default App;
